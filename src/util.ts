b75a653814e26d6ac3cc818b2d5f8693ae7e5729
Samrith Shankar <samrith@thetotem.co>
2020-01-29 01:59:40 +0100

    Added utils, bin and required runtime dependencies.

--- src/util.ts
+++ src/util.ts
@@ -1,11 +1,21 @@
-import * as path from "path";
-import * as fs from "fs";
+import { cursorTo, clearScreenDown } from "readline";
+import rc from "rc";
 
+import DEFAULT_CONFIG from "./default-config.json";
+
+/**
+ * Returns the loaded configuration object. Either from a .nodehawkrc or the default configuration file.
+ */
 export function loadConfiguration(): object {
-    try {
-        const userConfig = true;
-    } catch (error) {
-        throw error;
-    }
-    return {};
+    return rc("nodehawk", DEFAULT_CONFIG);
+}
+
+/**
+ * A simple function to clear the screen.
+ */
+export function clearScreen(): void {
+    const blank = "\n".repeat(process.stdout.rows);
+    console.log(blank);
+    cursorTo(process.stdout, 0, 0);
+    clearScreenDown(process.stdout);
 }

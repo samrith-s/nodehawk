573bc00e1f8039436c975c4fb961daf6a38502d8
Samrith Shankar <samrith@thetotem.co>
2020-01-30 15:20:39 +0100

    Used interface in util and created dummy implementation of processHandler

--- src/util.ts
+++ src/util.ts
@@ -1,17 +1,36 @@
 import { cursorTo, clearScreenDown } from "readline";
 import rc from "rc";
 
+import { Config } from "./interfaces";
+
 import DEFAULT_CONFIG from "./default-config.json";
 
 /**
  * Returns the loaded configuration object. Either from a .nodehawkrc or the default configuration file.
  */
-export function loadConfiguration(): object {
+export function loadConfiguration(): Config {
     return rc("nodehawk", DEFAULT_CONFIG);
 }
 
 /**
- * A simple function to clear the screen.
+ * Handles the cleanup and restart of the command executer.
+ *
+ * Kills the spawned process, kills the process running on the port to avoid conflicts and restarts a clean process.
+ * @param {boolean} initial Determines if this is the first time the process is running.
+ * @param {boolean} restart Determines if the process should restart after cleanup.
+ */
+export function processHandler(initial: boolean, restart: boolean): void {
+    if (initial) {
+        // do something
+    }
+
+    if (restart) {
+        // do something
+    }
+}
+
+/**
+ * Clears the terminal screen.
  */
 export function clearScreen(): void {
     const blank = "\n".repeat(process.stdout.rows);

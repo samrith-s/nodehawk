83c5701af788506c89acfdef76341b95461acc48
Samrith Shankar <samrith@thetotem.co>
2020-02-04 12:17:06 +0100

    Updated Nodehawk class.
    - Added jsdoc comment with example.
    - Added logs while loading and validating config.
    - Initialised first version of watcher in Nodehawk.

--- src/classes/Nodehawk.ts
+++ src/classes/Nodehawk.ts
@@ -10,14 +10,19 @@ import {
 } from "../utils";
 
 /**
- * The entry Nodehawk class.
+ * Nodehawk is a hyper configurable watcher for all of your Node server development needs.
+ * @example
+ * ```
+ * import Nodehawk from 'nodehawk';
+ * const nodehawk = new Nodehawk();
+ * ```
  */
 export class Nodehawk extends Provider {
     private watcher: Watcher;
     private checks: Checks;
 
     /**
-     * Create an instance of Nodehawk which
+     * Create an instance of `Nodehawk` which reads the configuration file and spawns a watcher to execute commands.
      */
     constructor() {
         const { config, checks }: ConfigAndChecks = getDefaultConfigAndChecks();
@@ -25,6 +30,14 @@ export class Nodehawk extends Provider {
 
         this.checks = checks;
 
+        this.log.info(
+            this.config.config
+                ? `Configuration loaded from ${this.config.config}`
+                : "No configuration found. Using default configurations."
+        );
         validateLoadedConfig({ config: this.config, checks: this.checks });
+        this.log.debug("Configuration verified.");
+
+        this.watcher = new Watcher(this.config);
     }
 }

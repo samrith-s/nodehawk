a85f653d1fd894cb9d38b46d3eae23c5a82a3171
Samrith Shankar <samrith@thetotem.co>
2020-02-16 00:25:09 +0100

    Implemented display class to manage spinner instead of using raw spinner.

---
+++ src/classes/Display.ts
@@ -0,0 +1,77 @@
+import Spinner, { Ora } from "ora";
+
+import { Config } from "../interfaces";
+
+/**
+ * The Display class is the primary class responsible for providing spinner visualisations.
+ */
+export class Display {
+    /**
+     * The configuration object passed to this instance.
+     */
+    private config: Config;
+
+    /**
+     * The `Ora` spinner instance which is useful for display purposes.
+     */
+    protected spinner: Ora;
+
+    /**
+     * A boolean to indicate if the process has started or not.
+     */
+    private started = false;
+
+    /**
+     * Create an instance of `Display`
+     * @param {Config} config The confguration object.
+     */
+    constructor(config: Config) {
+        this.config = config;
+        this.spinner = Spinner({
+            prefixText: "[Nodehawk]"
+        });
+    }
+
+    /**
+     * Display the `config.display.onBeforeStart` message with the spinner before executing.
+     */
+    public onBeforeStart(): void {
+        console.log("");
+        if (this.config.display.onBeforeStart) {
+            this.spinner = this.spinner.start(
+                this.config.display.onBeforeStart
+            );
+            this.started = true;
+        }
+    }
+
+    /**
+     * Display the `config.display.onStart` message with the spinner after executing.
+     */
+    public onStart(): void {
+        if (this.started && this.config.display.onStart) {
+            this.spinner = this.spinner.succeed(this.config.display.onStart);
+        }
+    }
+
+    /**
+     * Display the `config.display.onBeforeRestart` message with the spinner before restarting.
+     */
+    public onBeforeRestart(): void {
+        console.log("");
+        if (this.started && this.config.display.onBeforeRestart) {
+            this.spinner = this.spinner.start(
+                this.config.display.onBeforeRestart
+            );
+        }
+    }
+
+    /**
+     * Display the `config.display.onRestart` message with the spinner after restarting.
+     */
+    public onRestart(): void {
+        if (this.started && this.config.display.onRestart) {
+            this.spinner = this.spinner.succeed(this.config.display.onRestart);
+        }
+    }
+}

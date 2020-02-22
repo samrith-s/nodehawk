723d41be054b7c2eaa36c4b7520b4ef186730e82
Samrith Shankar <samrith@thetotem.co>
2020-02-22 11:41:17 +0100

    Initialised typescript example

---
+++ examples/express-typescript/src/server.ts
@@ -0,0 +1,14 @@
+import * as express from "express";
+import chalk from "chalk";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send(process.env.MY_MESSAGE);
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(
+        chalk`{yellow.bold App running on http://localhost:${process.env.PORT}}`
+    );
+});
